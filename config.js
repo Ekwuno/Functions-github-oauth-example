47c0d55b9691c36b3f957e052dc740f3761f2838
Ekwuno <obinnacodes@gmail.com>
2022-03-14 14:19:26 +0000

    adding config file

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+  CLIENT_ID: "d00bdb1af84c4faef82c",
+  CLIENT_SECRET: "5735f21e979685a52a7de10e30c936c661b565a5",
+  REDIRECT_URL: "http://localhost:8788/",
+  AUTHORIZATION_ENDPOINT: "https://github.com/login/oauth/authorize",
+  TOKEN_ENDPOINT: "https://github.com/login/oauth/access_token",
+  RESOURCE_ENDPOINT: "https://api.github.com/",
+};
