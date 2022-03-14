49b68e37fc7456b0875e9657c323a1eb71db7091
Ekwuno <obinnacodes@gmail.com>
2022-03-14 14:32:22 +0000

    update config secret

--- functions/api/code.js
+++ functions/api/code.js
@@ -20,7 +20,7 @@ export async function onRequestPost({ request, env }) {
 
 const config = {
   CLIENT_ID: "d00bdb1af84c4faef82c",
-  CLIENT_SECRET: "5735f21e979685a52a7de10e30c936c661b565a5",
+  CLIENT_SECRET: "16bf26fbf9fc5aad2f1eee3b94c5d6363d13470d",
   REDIRECT_URL: "http://localhost:8788/",
   AUTHORIZATION_ENDPOINT: "https://github.com/login/oauth/authorize",
   TOKEN_ENDPOINT: "https://github.com/login/oauth/access_token",
